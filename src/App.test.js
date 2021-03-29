ebc48f5f61a555f45378348538fff080b7b454d7
Laura McGowan <laura.mcgowan@bbc.co.uk>
2021-03-29 22:43:13 +0100

    feat(my-app): added new button and html

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import article1 from './data'
 
 test('renders learn react link', () => {
   render(<App />);
--- src/News.js
+++ src/News.js
@@ -8,11 +8,11 @@ export const News = () => {
   return (
     <>
       <div className="news-container"><h1>Welcome to the News!</h1></div>
-      <div className="news-container">
+      <div className="news-container"><button><h2>TOP STORIES</h2></button>
             
             <div>
-              {article1.title +
-                " , " +
+              {article1.title + 
+                ", " +
                 article1.body +
                 " ," +
                 article1.model +
@@ -20,10 +20,8 @@ export const News = () => {
                 article1.text}
             </div>
            
-        
       </div>
       
-      <button>Latest articles</button>
     </>
   );
 
