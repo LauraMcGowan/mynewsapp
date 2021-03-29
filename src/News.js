b99f8c2270b9ba844f5fe7b350e84725a1c8008a
Laura McGowan <laura.mcgowan@bbc.co.uk>
2021-03-29 14:46:45 +0100

    feat(my-app): added styling to news header

--- src/News.js
+++ src/News.js
@@ -4,14 +4,12 @@ import { article1 } from "./data/article-1";
 
 
 export const News = () => {
-    
+    <h1>Header</h1>
   return (
-     
     <>
-      <div className="news-container">Welcome to the News!</div>
+      <div className="news-container"><h1>Welcome to the News!</h1></div>
       <div className="news-container">
             
-              
             <div>
               {article1.title +
                 " , " +
@@ -22,7 +20,6 @@ export const News = () => {
                 article1.text}
             </div>
            
-        
         
       </div>
       
