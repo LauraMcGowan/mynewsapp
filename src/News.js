9d49879cd89a33262c0369e2962617d8df078dc4
Laura McGowan <laura.mcgowan@bbc.co.uk>
2021-03-31 16:11:24 +0100

    feat(my-app): added logic to display article and styling

--- src/News.js
+++ src/News.js
@@ -1,31 +1,53 @@
 import React from "react";
 import "./App.css";
 import { article1 } from "./data/article-1";
+import parse from "html-react-parser";
 
 
 
 export const News = () => {
-    <h1>Header</h1>
+
+const articleContent = article1.body.map(({type,model}) => {
+
+    let content = '';
+
+        switch (type) {
+            case "heading":
+                content =`<h4>${model.text}</h4>`
+                
+                break;
+
+                case "paragraph":
+                content =`<p>${model.text}</p>`
+                        
+                    
+                break;
+
+        
+                default:
+                break;
+        }
+
+    return content;
+
+}).join('')    
+
 
   return (
     <>
       <div className="news-container"><h1>Welcome to the News!</h1></div>
-      <div className="news-container"><h2>TOP STORIES</h2>
+      <div className="news-container"><h2>TOP STORIES</h2></div>
  
-
-      <div>
-
-    ));
-              {article1.title + 
-                ", " +
-                article1.body +
-                " ," +
-                article1.model +
-                ", " +
-                article1.text}
-            </div>
-           <h2><button>NEXT</button></h2><footer></footer>
-      </div>
+      <article>
+      <h3>{article1.title}</h3>    
+      {parse(articleContent)}
+    
+              
+     </article>
+    
+    <footer> 
+    <button>NEXT</button>
+    </footer>
       
     </>
   );
