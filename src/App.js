a2ca14522c536c3b0a22dec80fb78a9d1eb93648
Laura McGowan <laura.mcgowan@bbc.co.uk>
2021-03-31 11:29:06 +0100

    feat(my-app): added some styling config and added next button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { ReactComponent as logo } from './image.png';
 import { News } from "./News";
 
 
@@ -8,6 +9,7 @@ function App () {
   return (
     <div className="App">
       <News />
+      <logo />
     </div>
   );
 }
--- src/News.js
+++ src/News.js
@@ -3,14 +3,19 @@ import "./App.css";
 import { article1 } from "./data/article-1";
 
 
+
 export const News = () => {
     <h1>Header</h1>
+
   return (
     <>
       <div className="news-container"><h1>Welcome to the News!</h1></div>
-      <div className="news-container"><button><h2>TOP STORIES</h2></button>
-            
-            <div>
+      <div className="news-container"><h2>TOP STORIES</h2>
+ 
+
+      <div>
+
+    ));
               {article1.title + 
                 ", " +
                 article1.body +
@@ -19,7 +24,7 @@ export const News = () => {
                 ", " +
                 article1.text}
             </div>
-           
+           <h2><button>NEXT</button></h2><footer></footer>
       </div>
       
     </>
